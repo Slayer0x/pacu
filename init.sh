53bdd7ef91d614c76273d63b741fdfdc7f8bdb76
Slayer <76955086+Slayer0x@users.noreply.github.com>
2025-09-16 10:01:22 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,8 +6,7 @@ if [[ $EUID -ne 0 ]]; then
    echo "UID != 0" 
    exit 1
 fi
-packages="docker docker.io docker-compose"
-DEBIAN_FRONTEND=noninteractive apt-get -y update && apt-get -y dist-upgrade && apt-get install -y $packages && apt-get autoremove -y && apt-get clean -y && rm -rf /var/lib/apt/lists/*
+DEBIAN_FRONTEND=noninteractive apt-get -y update && apt-get -y dist-upgrade && apt-get install docker docker.io docker-compose -y && apt-get autoremove -y && apt-get clean -y && rm -rf /var/lib/apt/lists/*
 for image in evilginx nginx-proxy gophish
 do
     docker pull ghcr.io/thirdbyte/pacu:$image
